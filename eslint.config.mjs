946aa5996d3fbbed95a1e78d8c87d82be538005b
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-11-01 20:21:19 +0700

    feat: update eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,32 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/prefer-optional-chain": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "off",
+      "@typescript-eslint/no-misused-promises": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/consistent-indexed-object-style": "off",
+      "@typescript-eslint/prefer-promise-reject-errors": "off",
+      "@typescript-eslint/non-nullable-type-assertion-style": "off",
+      "@typescript-eslint/await-thenable": "off",
+      "@typescript-eslint/no-redundant-type-constituents": "off",
+      "@next/next/no-img-element": "off",
+      "prefer-const": "off",
+      "react/jsx-key": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
